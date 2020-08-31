3305ffd9e8fc514ccc4c84013fff0af2e7b579c2
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-09-01 03:28:46 +0600

    Make changes in Banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,7 +8,7 @@ function Banner() {
       <div className="container text-center">
         <h1 className="display-4 p-3">𝕎𝕖𝕝𝕔𝕠𝕞𝕖 𝕥𝕠 𝕥𝕙𝕖 𝕔𝕠𝕦𝕟𝕥𝕣𝕪'𝕤 𝕟𝕠. 𝟙 𝕖-𝕤𝕙𝕠𝕡</h1>
         <hr className="w-50" />
-        <div className="row my-4 d-flex justify-content-center">
+        <div className="row mt-5 mb-4 d-flex justify-content-center">
           <div className="card col-md-3">
             <img
               src="https://i.ibb.co/jr6TksZ/product-1.png"
