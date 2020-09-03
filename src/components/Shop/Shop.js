ad93484ab69886d9392b0e326f8545cb48d00051
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-09-03 22:13:23 +0600

    Add Shop component

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./Shop.css";
 import Products from "../Products/Products";
-import FeaturedProducts from "../FeaturedProducts/FeaturedProducts";
-import Newsletter from "../Newsletter/Newsletter";
-import MoreProducts from "../MoreProducts/MoreProducts";
+import { Link } from "react-router-dom";
 
 function Shop() {
   return (
-    <div id="shop">
-      <h1>Your e-shop</h1>
+    <div className="shop">
+      <div className="shop-header text-center">
+        <h2 className="display-4 mt-3">𝕃𝕖𝕥'𝕤 𝕤𝕥𝕒𝕣𝕥 𝕤𝕙𝕠𝕡𝕡𝕚𝕟𝕘</h2>
+        <hr />
+        <h1>Categories</h1>
+        <nav className="category-navbar">
+          <a href="#laptops">Laptops</a> |<a href="#mobiles">Mobiles</a> |
+          <a href="#bags">Bags</a>
+        </nav>
+        <hr />
+      </div>
       <Products />
-      <FeaturedProducts />
-      <Newsletter />
-      <MoreProducts />
+      <footer className="text-center">
+        <nav>
+          <Link to="/">Home</Link> |<Link to="/cart">Cart</Link> |
+          <Link to="/help">Help</Link>
+        </nav>
+        <p>Copyright © 2020 e-shop. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
