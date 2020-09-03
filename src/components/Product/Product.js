c3d304545d8d3671f8551a4830959c8baf207708
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-09-03 22:21:09 +0600

    Add Product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,22 @@
 import React from "react";
+import "./Product.css";
 
-function Product() {
+function Product(props) {
+  const { image, title, price } = props.product;
   return (
-    <div>
-      <h1>Single Product</h1>
+    <div className="col-md-4 product my-3">
+      <div className="card">
+        <img src={image} className="card-img-top" alt="" />
+        <div className="card-body d-flex justify-content-between">
+          <div>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">Price: ${price}</p>
+          </div>
+          <div className="pt-4">
+            <a className="btn btn-dark">Add to cart</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
